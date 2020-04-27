204b0b1c808611e5d8b54a41a4ae6ffcd3de60fe
pe-taro <63912337+pe-taro@users.noreply.github.com>
2020-04-27 21:14:47 +0900

    Update app.js

--- build/app.js
+++ build/app.js
@@ -1,25 +1,26 @@
 var c = document.createElement("canvas");
 var ctx = c.getContext("2d");
-c.width = 1000;
-c.height = 500;
+// c.width = window.innerWidth;
+// c.height = window.innerHeight;
+c.width = 600
+c.height = 400;
 
-var size = 14;
+var size = 15;
 
 document.body.appendChild(c);
 
 var perm = [];
 
 while (perm.length < 255) {
-  while (perm.includes(val = Math.floor(Math.random()*255)));
+  while (perm.includes(val = Math.floor(Math.random() * 255)));
   perm.push(val);
 }
 
 var lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t * Math.PI)) / 2;
 
-
 var noise = x => {
   x = x * 0.01 % 255;
-  return lerp(perm[Math.floor(x)],perm[Math.ceil(x)], x - Math.floor(x));
+  return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 }
 
 var player = new function() {
@@ -31,44 +32,46 @@ var player = new function() {
 
   this.img = new Image();
   this.img.src = "images/moto.png";
+  // this.img.src = "images/trump.png";
 
   this.draw = function() {
-var p1 = c.height - noise(t + this.x) * 0.25;
-var p2 = c.height - noise(t + 5 + this.x) * 0.25;
+    var p1 = c.height - noise(t + this.x) * 0.25;
+    var p2 = c.height - noise(t + 5 + this.x) * 0.25;
 
-var grounded = 0
+    var grounded = 0
 
-if(p1 - size > this.y) {
-  this.ySpeed += 0.1;
-}else {
-  this.ySpeed -= this.y - (p1 - size);
-  this.y = p1 - size;
+    if(p1 - size > this.y) {
+      this.ySpeed += 0.1;
+    } else {
+      this.ySpeed -= this.y - (p1 - size);
+      this.y = p1 - size;
 
-  grounded = 1;
-}
+      grounded = 1;
+    }
 
-if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5) {
-  playing = false;
-  this.rSpeed = 5;
-  k.ArrowUp = 1;
-  this.x -= speed * 5;
-}
+    if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5) {
+      playing = false;
+      this.rSpeed = 5;
+      k.ArrowUp = 1;
+      this.x -= speed * 5;
+    }
 
-var angle = Math.atan2((p2 - size) - this.y, (this.x + 5) - this.x);
+    var angle = Math.atan2((p2 - size) - this.y, (this.x + 5) - this.x);
 
-// this.rot = angle;
-this.y += this.ySpeed;
+    // this.rot = angle;
 
-if(grounded && playing) {
-  this.rot -= (this.rot - angle) * 0.5;
-  this.rSpeed = this.rSpeed - (angle - this.rot);
-}
+    this.y += this.ySpeed;
 
-this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
-this.rot -= this.rSpeed * 0.1;
+    if(grounded && playing) {
+      this.rot -= (this.rot - angle) * 0.5;
+      this.rSpeed = this.rSpeed - (angle - this.rot);
+    }
 
-if(this.rot > Math.PI) this.rot = -Math.PI;
-if(this.rot < -Math.PI) this.rot = Math.PI;
+    this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
+    this.rot -= this.rSpeed * 0.1;
+
+    if(this.rot > Math.PI) this.rot = -Math.PI;
+    if(this.rot < -Math.PI) this.rot = Math.PI;
 
 
     ctx.save();
@@ -76,20 +79,20 @@ if(this.rot < -Math.PI) this.rot = Math.PI;
     ctx.rotate(this.rot);
     ctx.drawImage(this.img, -size, -size, 30, 30);
 
-  ctx.restore();
+    ctx.restore();
   }
 }
 
 var t = 0;
 var speed = 0;
-var playing =true;
-var k = {ArrowUp: 1, ArrowDown: 0, ArrowLeft: 0, ArrowRight: 0};
+var playing = true;
+var k = {ArrowUp: 0, ArrowDown: 0, ArrowLeft: 0, ArrowRight: 0};
 
 
 function loop() {
   speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.1;
   t += 10 * speed;
-  ctx.fillStyle = "#f92";
+  ctx.fillStyle = "#19f";
   ctx.fillRect(0, 0, c.width, c.height);
 
   ctx.fillStyle = "black";
@@ -98,19 +101,18 @@ function loop() {
   ctx.moveTo(0, c.height);
 
   for (var i = 0; i < c.width; i++) {
-    ctx.lineTo(i, c.height - noise(t + i) * 0.25);    
+    ctx.lineTo(i, c.height - noise(t + i) * 0.25);
   }
 
   ctx.lineTo(c.width, c.height);
 
   ctx.fill();
-  
+
   player.draw();
   requestAnimationFrame(loop);
 }
 
 onkeydown = d => k[d.key] = 1;
-onkeyup = d=> k[d.key]  = 0;
-
+onkeyup = d => k[d.key] = 0;
 
 loop();
